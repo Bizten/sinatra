58df7e524b0515d4ecdf6b752ce7ca77648525c6
chedice <chedice@proton.me>
2022-10-16 10:26:20 +0000

    update Gemfile

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,10 @@
 class ClientsController < Sinatra::Base
+  configure do
+    set :session_secret, 'password_security'
+  end
   has_valid_card has_one :card
+
+
 
   class Appointment < ActiveRecord::Base
     belongs_to :client
